b054872726b9ebbd16f0131390472e9163e20f0a
Mk <miriankakhidze@gmail.com>
2023-07-19 03:02:42 +0400

    fix

--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -2,6 +2,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { type NextRequest, NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
   const supabase = createRouteHandlerClient({ cookies })
