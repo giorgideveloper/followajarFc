de8dce71f72f368b2c2de7b79c0c93e2528f74e9
Mk <miriankakhidze@gmail.com>
2023-07-28 13:24:53 +0400

    fix

--- src/app/auth/reset/route.ts
+++ src/app/auth/reset/route.ts
@@ -1,31 +1,20 @@
-import { createRouteHandlerClient, createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { NextApiRequest, NextApiResponse } from 'next';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 export const dynamic = 'force-dynamic';
 
-export async function GET(req: NextRequest, res: NextApiResponse) {
-  // const res = NextResponse.next()
+export async function GET(req: NextRequest) {
   const requestUrl = new URL(req.url)
   const code = requestUrl.searchParams.get('code')
-  console.log(code);
- 
-
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies })
-   const {data,error}= await supabase.auth.exchangeCodeForSession(code)
-   console.log(data,error);
-   
+    const supabase = createRouteHandlerClient({ cookies: () => cookies() })
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code)
+    console.log(data, error);
+
   }
 
   const next = requestUrl.searchParams.get('next')
-  console.log(next);
 
-  // using NextJS API response object in this example
-  // res.redirect(next)
-  // URL to redirect to after sign in process completes
-  //@ts-ignore
-  return NextResponse.redirect(next)
+  return NextResponse.redirect(requestUrl.origin + next)
 }
