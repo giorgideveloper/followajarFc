0b37fc5343bf89db27aa6dfd42e1034adab893f8
Mk <miriankakhidze@gmail.com>
2023-07-28 13:29:59 +0400

    fix

--- src/app/auth/reset/route.ts
+++ src/app/auth/reset/route.ts
@@ -1,7 +1,7 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
