e69bdb0384b4dbb2d514b80157e7f47dfaeec9e5
Mk <miriankakhidze@gmail.com>
2023-07-31 23:36:20 +0400

    fix

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -24,8 +24,8 @@ export default async function Page() {
         <div className="w-full lg:w-3/4 mx-auto p-6">
             <div className="card w-full lg:w-2/3 bg-base-100 shadow-xl mx-auto mt-6">
                 <div className="card-body">
-                    <p className="text-2xl font-bold text-center my-10 text-red-600">რეგისტრაცია დაიწყება 1 აგვისტოს!</p>
-                    {/* <RegisterForm /> */}
+                    {/* <p className="text-2xl font-bold text-center my-10 text-red-600">რეგისტრაცია დაიწყება 1 აგვისტოს!</p> */}
+                    <RegisterForm />
                 </div>
             </div>
         </div>
