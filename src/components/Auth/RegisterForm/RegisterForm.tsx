1c8eb31060d58940490a0d94074cd4c3a3a0ace7
Mk <miriankakhidze@gmail.com>
2023-07-31 15:52:42 +0400

    fix

--- src/components/Auth/RegisterForm/RegisterForm.tsx
+++ src/components/Auth/RegisterForm/RegisterForm.tsx
@@ -7,24 +7,58 @@ import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { FC, useState } from "react"
 import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { ref } from "yup"
 
 
-interface IFormInput {
-    name: string
-    lastname: string
-    email: string
-    birthday: string
-    tel: string
-    personalNumber: string
-    password: string
-    rePassword: string
-    avatar_url: string
-    fb: string
-    ig: string
-    yt: string
-    tk: string
-    rules: boolean
-}
+const getCharacterValidationError = (str: string) => {
+    return `პაროლი უნდა შეიცავდეს მინიმუმ 1 ${str}`;
+};
+
+const schema = yup
+    .object({
+        name: yup.string().required('* აუცილებელი ველი'),
+        lastname: yup.string().required('* აუცილებელი ველი'),
+        email: yup.string().email('არასწორი ფორმატი').required('* აუცილებელი ველი'),
+        birthday: yup.date().required('* აუცილებელი ველი'),
+        tel: yup.string().required('* აუცილებელი ველი'),
+        personalNumber: yup.string().length(11, 'პირადი ნომერი უნდა შედგებოდეს 11 ციფრისაგან!').matches(/^[0-9]+$/, 'გამოიყენეთ მხოლოდ ციფრები!').required('* აუცილებელი ველი'),
+        password: yup.string()
+            .required("* შეიყვანეთ პაროლი")
+            .min(8, "პაროლი უნდა შედგებოდეს მინუმუმ 8 სიმბოლოსგან")
+            .matches(/[0-9]/, getCharacterValidationError("ციფრს"))
+            .matches(/[a-z]/, getCharacterValidationError("პატარა ასოს"))
+            .matches(/[A-Z]/, getCharacterValidationError("დიდ ასოს")),
+        rePassword: yup.string()
+            .required("* გაიმეორეთ პაროლი")
+            .oneOf([ref("password")], "პაროლები არ ემთხვევა"),
+        fb: yup.string(),
+        ig: yup.string(),
+        yt: yup.string(),
+        tk: yup.string(),
+        avatar_url: yup.string().required('* აუცილებელი ველი'),
+        rules: yup.boolean().required('* გთხოვთ დაეთანხმოთ წესებს და პირობებს'),
+    })
+    .required()
+
+
+// interface IFormInput {
+//     name: string
+//     lastname: string
+//     email: string
+//     birthday: string
+//     tel: string
+//     personalNumber: string
+//     password: string
+//     rePassword: string
+//     avatar_url: string
+//     fb: string
+//     ig: string
+//     yt: string
+//     tk: string
+//     rules: boolean
+// }
 
 interface RegisterFormProps {
 }
@@ -43,24 +77,25 @@ const RegisterForm: FC<RegisterFormProps> = () => {
         watch,
         formState: { errors },
         reset
-    } = useForm<IFormInput>({
-        defaultValues: {
-            // name: 'John',
-            // lastname: 'Doe',
-            // email: 'mkakh1dz3@gmail.com',
-            // password: '1qazXSW@',
-            // rePassword: '1qazXSW@',
-            // birthday: 'string',
-            // tel: 'string',
-            // personalNumber: '61006055412',
-            // fb: '',
-            // ig: '',
-            // yt: '',
-            // tk: ''
-        }
+    } = useForm({
+        resolver: yupResolver(schema),
+        // defaultValues: {
+        //     // name: 'John',
+        //     // lastname: 'Doe',
+        //     // email: 'mkakh1dz3@gmail.com',
+        //     // password: '1qazXSW@',
+        //     // rePassword: '1qazXSW@',
+        //     // birthday: 'string',
+        //     // tel: 'string',
+        //     // personalNumber: '61006055412',
+        //     // fb: '',
+        //     // ig: '',
+        //     // yt: '',
+        //     // tk: ''
+        // }
     })
 
-    const onSubmit: SubmitHandler<IFormInput> = async (values) => {
+    const onSubmit: SubmitHandler<any> = async (values) => {
         const { email, password, rules, ...rest } = values
 
         try {
@@ -118,8 +153,10 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                             name="name"
                             control={control}
                             rules={{ required: true }}
-                            render={({ field }) => <Input {...field} label="სახელი" placeholder="სახელი" />}
-                        /> {errors.name && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                            render={({ field }) => <Input {...field} label="სახელი" placeholder="სახელი" aria-invalid={errors.name ? "true" : "false"} />}
+                        />
+                        {/* {errors.name && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>} */}
+                        {errors.name && <span className="text-red-700 text-sm mt-2">{errors.name.message}</span>}
                     </div>
 
                     <div className="w-full">
@@ -128,7 +165,10 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                             control={control}
                             rules={{ required: true }}
                             render={({ field }) => <Input {...field} label="გვარი" placeholder="გვარი" />}
-                        /> {errors.lastname && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                        />
+                        {/* {errors.lastname && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>} */}
+                        {errors.lastname && <span className="text-red-700 text-sm mt-2">{errors.lastname.message}</span>}
+
                     </div>
 
                 </div>
@@ -139,7 +179,8 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                             control={control}
                             rules={{ required: true }}
                             render={({ field }) => <Input {...field} type="email" label="ელ. ფოსტა" placeholder="ელ. ფოსტა" />}
-                        /> {errors.email && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                        />
+                        {errors.email && <span className="text-red-700 text-sm mt-2">{errors.email.message}</span>}
                     </div>
                     <div className="w-full">
                         <Controller
@@ -147,7 +188,8 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                             control={control}
                             rules={{ required: true }}
                             render={({ field }) => <Input {...field} type="date" label="დაბადების თარიღი" placeholder="დაბადების თარიღი" />}
-                        /> {errors.birthday && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                        />
+                        {errors.birthday && <span className="text-red-700 text-sm mt-2">{errors.birthday.message}</span>}
                     </div>
                 </div>
                 <div className="flex flex-col md:flex-row gap-4">
@@ -157,15 +199,18 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                             control={control}
                             rules={{ required: true }}
                             render={({ field }) => <Input {...field} type="tel" label="ტელეფონი" placeholder="ტელეფონი" />}
-                        /> {errors.tel && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                        />
+                        {errors.tel && <span className="text-red-700 text-sm mt-2">{errors.tel.message}</span>}
+                        {/* {errors.tel && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>} */}
                     </div>
                     <div className="w-full">
                         <Controller
                             name="personalNumber"
                             control={control}
                             rules={{ required: true, }}
-                            render={({ field }) => <Input {...field} type="text" label="პირადი ნომერი" placeholder="პირადი ნომერი" />}
-                        /> {errors.personalNumber && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                            render={({ field }) => <Input {...field} type="text" label="პირადი ნომერი" placeholder="პირადი ნომერი" aria-invalid={errors.personalNumber ? "true" : "false"} />}
+                        />
+                        {errors.personalNumber && <span className="text-red-700 text-sm mt-2">{errors.personalNumber?.message}</span>}
                     </div>
                 </div>
 
@@ -176,7 +221,8 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                             control={control}
                             rules={{ required: true, minLength: 6 }}
                             render={({ field }) => <Input {...field} type="password" label="პაროლი" placeholder="პაროლი" />}
-                        /> {errors.password && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                        />
+                        {errors.password && <span className="text-red-700 text-sm mt-2">{errors.password.message}</span>}
                     </div>
                     <div className="w-full">
                         <Controller
@@ -184,36 +230,36 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                             control={control}
                             rules={{ required: true, minLength: 6 }}
                             render={({ field }) => <Input {...field} type="password" label="გაიმეორეთ პაროლი" placeholder="პაროლი" />}
-                        /> {errors.rePassword && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                        />
+                        {errors.rePassword && <span className="text-red-700 text-sm mt-2">{errors.rePassword.message}</span>}
                     </div>
                 </div>
                 <Controller
                     name="fb"
                     control={control}
                     render={({ field }) => <Input {...field} type="text" label="Facebook" placeholder="https://www.facebook.com/*********/" />}
-                /> {errors.fb && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                /> {errors.fb && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>}
                 <Controller
                     name="ig"
                     control={control}
                     render={({ field }) => <Input {...field} type="text" label="Instagram" placeholder="https://www.instagram.com/*********/" />}
-                /> {errors.ig && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                /> {errors.ig && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>}
                 <Controller
                     name="yt"
                     control={control}
                     render={({ field }) => <Input {...field} type="text" label="Youtube" placeholder="https://www.youtube.com/@*********/" />}
-                /> {errors.yt && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                /> {errors.yt && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>}
                 <Controller
                     name="tk"
                     control={control}
                     render={({ field }) => <Input {...field} type="text" label="Tiktok" placeholder="https://www.tiktok.com/@*********/" />}
-                /> {errors.tk && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
+                /> {errors.tk && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>}
 
                 <Controller
                     name="avatar_url"
                     control={control}
                     rules={{ required: true }}
                     render={({ field }) => <Input
-                        // {...field}
                         type="file"
                         label="სურათი"
                         placeholder="სურათი"
@@ -230,15 +276,17 @@ const RegisterForm: FC<RegisterFormProps> = () => {
                         }
                         }
                     />}
-                /> {errors.avatar_url && <span className="text-red-700 mt-2">* აუცილებელი ველი</span>}
-
+                />
+                {errors.avatar_url && <span className="text-red-700 text-sm mt-2">{errors.avatar_url.message}</span>}
 
                 <Controller
                     name="rules"
                     control={control}
                     rules={{ required: true }}
                     render={({ field: { value, ...rest } }) => <Checkbox {...rest} label="ვეთანხმები წესებს და პირობებს" />}
-                />{errors.rules && <span className="text-red-700 mt-2">* გთხოვთ დაეთანხმოთ წესებს და პირობებს</span>}
+                />
+                {errors.rules && <span className="text-red-700 text-sm mt-2">{errors.rules.message}</span>}
+                {/* {errors.rules && <span className="text-red-700 mt-2">* გთხოვთ დაეთანხმოთ წესებს და პირობებს</span>} */}
 
                 <div>
                     <button className="btn btn-block btn-primary" type='submit' disabled={loading}>
