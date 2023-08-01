a4e53b1b0b79ee521b7d857db89875668be2fe91
Mk <miriankakhidze@gmail.com>
2023-08-01 17:16:35 +0400

    fix

--- src/components/TourPackage/TourPackage.tsx
+++ src/components/TourPackage/TourPackage.tsx
@@ -9,6 +9,21 @@ const TourPackage = ({ id }: { id: string }) => {
 
     return (
         <div className="max-w-full text-justify mt-10">
+            <p>
+                გილოცავთ, თქვენ წარმატებით გაიარეთ რეგისტრაცია.
+            </p>
+            <p className='my-3'>
+                კამპანიაში ჩასართავად, 15 აგვისტოდან - 15 ოქტომბრის ჩათვლით, თქვენს მიერ წინასწარ დარეგისტრირებულ პლატფორმებზე (Facebook, YouTube, Instagram, Tik-Tok)  უნდა განათავსოთ ,,ტურისტულ პაკეტში” შემავალ თითოეულ პუნქტთან დაკავშირებით ერთი პოსტი მაინც. ჰეშთეგით - #გამომყევიაჭარაში
+            </p>
+            <p className='mb-3'>
+
+                ტურისტულ პროდუქტებთან დაკავშირებით დამატებითი ინფორმაციის მისაღებად, დაუკავშირდით ტურისტულ საინფორმაციო ცენტრს:  577 90 90 93
+            </p>
+
+            <p>
+                წარმატებებს გისურვებთ.
+            </p>
+            <div className="divider"></div>
             <h1 className="text-center text-xl">{romans[parseInt(id) - 1]} პაკეტის ინფორმაცია</h1>
             <div className='flex flex-wrap flex-row gap-5 mt-10 justify-center'>
 
