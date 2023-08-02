3ecfbc3b2865190fc90dc0f86453010b048ec6fc
Kajaia <lashakajaia31@gmail.com>
2023-08-02 15:39:00 +0400

    Changed registration form layout and styling

--- src/components/Auth/RegisterForm/RegisterForm.tsx
+++ src/components/Auth/RegisterForm/RegisterForm.tsx
@@ -274,6 +274,23 @@ const RegisterForm: FC<RegisterFormProps> = () => {
         className=" w-full space-y-4 my-4"
         onSubmit={handleSubmit(onSubmit)}
       >
+        <h3 className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          <strong className="ml-2">პირადი ინფორმაცია</strong>
+        </h3>
         <div className="flex flex-col md:flex-row gap-4">
           <div className="w-full">
             <Controller
@@ -325,25 +342,25 @@ const RegisterForm: FC<RegisterFormProps> = () => {
         <div className="flex flex-col md:flex-row gap-4">
           <div className="w-full">
             <Controller
-              name="email"
+              name="personalNumber"
               control={control}
               rules={{ required: true }}
               render={({ field }) => (
                 <Input
                   {...field}
-                  type="email"
-                  label="ელ. ფოსტა"
-                  placeholder="მაგ: giorgiberidze@mail.com"
+                  type="text"
+                  label="პირადი ნომერი"
+                  placeholder="მაგ: 61001010010"
+                  aria-invalid={errors.personalNumber ? "true" : "false"}
                 />
               )}
             />
-            {errors.email && (
+            {errors.personalNumber && (
               <span className="text-red-700 text-sm mt-2">
-                {errors.email.message}
+                {errors.personalNumber?.message}
               </span>
             )}
           </div>
-
           <div className="w-full">
             <Controller
               name="birthday"
@@ -361,50 +378,6 @@ const RegisterForm: FC<RegisterFormProps> = () => {
             {errors.birthday && (
               <span className="text-red-700 text-sm mt-2">
                 {errors.birthday.message}
-              </span>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col md:flex-row gap-4">
-          <div className="w-full">
-            <Controller
-              name="tel"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  type="tel"
-                  label="ტელეფონი"
-                  placeholder="მაგ: 555112233"
-                />
-              )}
-            />
-            {errors.tel && (
-              <span className="text-red-700 text-sm mt-2">
-                {errors.tel.message}
-              </span>
-            )}
-            {/* {errors.tel && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>} */}
-          </div>
-          <div className="w-full">
-            <Controller
-              name="personalNumber"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  type="text"
-                  label="პირადი ნომერი"
-                  placeholder="მაგ: 61001010010"
-                  aria-invalid={errors.personalNumber ? "true" : "false"}
-                />
-              )}
-            />
-            {errors.personalNumber && (
-              <span className="text-red-700 text-sm mt-2">
-                {errors.personalNumber?.message}
               </span>
             )}
           </div>
@@ -451,66 +424,180 @@ const RegisterForm: FC<RegisterFormProps> = () => {
             )}
           </div>
         </div>
-        <Controller
-          name="fb"
-          control={control}
-          render={({ field }) => (
-            <Input
-              {...field}
-              type="text"
-              label="Facebook"
-              placeholder="https://www.facebook.com/*********/"
+        <h3 className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
             />
-          )}
-        />{" "}
-        {errors.fb && (
-          <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>
-        )}
-        <Controller
-          name="ig"
-          control={control}
-          render={({ field }) => (
-            <Input
-              {...field}
-              type="text"
-              label="Instagram"
-              placeholder="https://www.instagram.com/*********/"
+          </svg>
+          <strong className="ml-2">საკონტაქტო ინფორმაცია</strong>
+        </h3>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="w-full">
+            <Controller
+              name="tel"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="tel"
+                  label="ტელეფონი"
+                  placeholder="მაგ: 555112233"
+                />
+              )}
             />
-          )}
-        />{" "}
-        {errors.ig && (
-          <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>
-        )}
-        <Controller
-          name="yt"
-          control={control}
-          render={({ field }) => (
-            <Input
-              {...field}
-              type="text"
-              label="Youtube"
-              placeholder="https://www.youtube.com/@*********/"
+            {errors.tel && (
+              <span className="text-red-700 text-sm mt-2">
+                {errors.tel.message}
+              </span>
+            )}
+            {/* {errors.tel && <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>} */}
+          </div>
+          <div className="w-full">
+            <Controller
+              name="email"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="email"
+                  label="ელ. ფოსტა"
+                  placeholder="მაგ: giorgiberidze@mail.com"
+                />
+              )}
             />
-          )}
-        />{" "}
-        {errors.yt && (
-          <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>
-        )}
-        <Controller
-          name="tk"
-          control={control}
-          render={({ field }) => (
-            <Input
-              {...field}
-              type="text"
-              label="Tiktok"
-              placeholder="https://www.tiktok.com/@*********/"
+            {errors.email && (
+              <span className="text-red-700 text-sm mt-2">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+        </div>
+        <h3 className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
             />
-          )}
-        />{" "}
-        {errors.tk && (
-          <span className="text-red-700 text-sm mt-2">* აუცილებელი ველი</span>
-        )}
+          </svg>
+          <strong className="ml-2">სოციალური ქსელები</strong>
+        </h3>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="w-full">
+            <Controller
+              name="fb"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="text"
+                  label="Facebook"
+                  placeholder="https://www.facebook.com/*********/"
+                />
+              )}
+            />{" "}
+            {errors.fb && (
+              <span className="text-red-700 text-sm mt-2">
+                * აუცილებელი ველი
+              </span>
+            )}
+          </div>
+          <div className="w-full">
+            <Controller
+              name="ig"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="text"
+                  label="Instagram"
+                  placeholder="https://www.instagram.com/*********/"
+                />
+              )}
+            />{" "}
+            {errors.ig && (
+              <span className="text-red-700 text-sm mt-2">
+                * აუცილებელი ველი
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="w-full">
+            <Controller
+              name="yt"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="text"
+                  label="YouTube"
+                  placeholder="https://www.youtube.com/@*********/"
+                />
+              )}
+            />{" "}
+            {errors.yt && (
+              <span className="text-red-700 text-sm mt-2">
+                * აუცილებელი ველი
+              </span>
+            )}
+          </div>
+          <div className="w-full">
+            <Controller
+              name="tk"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="text"
+                  label="TikTok"
+                  placeholder="https://www.tiktok.com/@*********/"
+                />
+              )}
+            />{" "}
+            {errors.tk && (
+              <span className="text-red-700 text-sm mt-2">
+                * აუცილებელი ველი
+              </span>
+            )}
+          </div>
+        </div>
+        <h3 className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+          <strong className="ml-2">თქვენი ფოტო</strong>
+        </h3>
         <Controller
           name="avatar_url"
           control={control}
@@ -518,8 +605,7 @@ const RegisterForm: FC<RegisterFormProps> = () => {
           render={({ field }) => (
             <Input
               type="file"
-              label="სურათი"
-              placeholder="სურათი"
+              label=""
               className="file-input file-input-bordered w-full"
               onChange={async (e: any) => {
                 setLoading(true);
