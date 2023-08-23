acac52f46791f1c417fd5d941d400e5cb57ed8b0
Mk <miriankakhidze@gmail.com>
2023-08-23 21:14:15 +0400

    fix

--- src/components/SocialFeeds/SocialFeeds.tsx
+++ src/components/SocialFeeds/SocialFeeds.tsx
@@ -12,8 +12,8 @@ const SocialFeeds = () => {
             <div className="divider"></div>
 
             <div className="tabs ml-10">
-                <a className={`tab tab-lg ${index === 0 ? 'tab-active' : ''}`} onClick={() => setIndex(0)}>Instagram</a>
-                <a className={`tab tab-lg ${index === 1 ? 'tab-active' : ''}`} onClick={() => setIndex(1)}>Facebook</a>
+                <a className={`tab tab-lg ${index === 0 ? 'tab-active' : ''}`} onClick={() => setIndex(0)}>Facebook</a>
+                <a className={`tab tab-lg ${index === 1 ? 'tab-active' : ''}`} onClick={() => setIndex(1)}>Instagram</a>
                 <a className={`tab tab-lg ${index === 2 ? 'tab-active' : ''}`} onClick={() => setIndex(2)}>Tiktok</a>
             </div>
 
@@ -28,12 +28,14 @@ const Tabs = ({ index }: { index: number }) => {
 
     return (
         <>
-            <div className="elfsight-app-2bcd1b8a-7d77-4651-8775-3acc1d7ab645" style={{ display: index == 0 ? 'block' : 'none' }}></div>
-            <div className='sk-ww-facebook-hashtag-feed' data-embed-id='184031' style={{ display: index == 1 ? 'block' : 'none' }}></div>
+            <iframe src='https://widgets.sociablekit.com/facebook-hashtag-posts/iframe/184031' width='100%' height='550' style={{ display: index == 0 ? 'block' : 'none' }}></iframe>
+            {/* <div className='sk-ww-facebook-hashtag-feed' data-embed-id='184031' style={{ display: index == 0 ? 'block' : 'none' }}></div> */}
+            <div className="elfsight-app-2bcd1b8a-7d77-4651-8775-3acc1d7ab645" style={{ display: index == 1 ? 'block' : 'none' }}></div>
             <div className="elfsight-app-7761cb95-401c-46e4-8ec7-6548960b1308" style={{ display: index == 2 ? 'block' : 'none' }}></div>
 
+
             <Script src='https://static.elfsight.com/platform/platform.js' data-use-service-core defer></Script>
-            <Script src='https://widgets.sociablekit.com/facebook-hashtag-posts/widget.js' async defer></Script>
+            {/* <Script src='https://widgets.sociablekit.com/facebook-hashtag-posts/widget.js' async defer></Script> */}
             <Script src='https://static.elfsight.com/platform/platform.js' data-use-service-core defer></Script>
         </>
     )
