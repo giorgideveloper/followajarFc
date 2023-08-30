b866c04fc9a4baf2de71ff2cbb2cac6bb3d3e0e6
Jorjo <gorgiladze888@gmail.com>
2023-08-31 01:33:21 +0400

    add type editobject

---
+++ src/components/EditObj/ObjEditForm.interface.ts
@@ -0,0 +1,19 @@
+export interface ObjEditFormType {
+	object_name: string;
+	object_type: number;
+	name: string;
+	last_name: string;
+	address: string;
+	id_number: Blob;
+	email: string;
+	mobile: string;
+	time_from: null;
+	time_to: null;
+	discount: number;
+	facebook: string;
+	instagram: string;
+	description: string;
+	password: string;
+	images: object;
+	image: string;
+}
